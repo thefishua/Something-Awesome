697e64eb78b8788a40a2db9000eea70616ce1e10
Joshua Fish <josh.fishua@gmail.com>
2022-03-28 00:40:04 +1100

    Added raspizero_hid files to the Something Awesome to enable scripting

---
+++ HID/utils/windows/cygwin/__init__.py
@@ -0,0 +1,60 @@
+# encoding=utf-8
+import os
+import time
+import re
+
+from tqdm import tqdm
+
+import HID
+from HID.REPORTS import CHARS
+
+
+def c_drive():
+    HID.type_string('cd /cydrive/c\n')
+
+
+def mkdir(filepath, recursive=True):
+    HID.type_string('mkdir -p {}\n'.format(filepath))
+
+
+def vim(filepath, insert_mode=False):
+    HID.type_string('vi {}\n'.format(filepath))
+
+    if insert_mode:
+        time.sleep(0.5)
+        HID.type_string('i')
+        time.sleep(0.2)
+    else:
+        time.sleep(0.5)
+
+
+def vim_wq():
+    HID.press(CHARS['ESC'])
+    time.sleep(0.5)
+    HID.type_string(':wq\n')
+
+
+def type_file_to_vim(filepath, where=None):
+    assert os.path.exists(filepath)
+
+    if where:
+        HID.type_string('cd {}\n'.format(where))
+
+    parent_dir, filename = os.path.split(filepath)
+    mkdir(parent_dir)
+    time.sleep(0.2)
+    vim(filepath, insert_mode=True)
+    # get line count for progress bar
+    num_lines = HID.utils.line_count(filepath)
+    # open file to read content
+    try:
+        with open(filepath, encoding='utf-8') as inp_file:
+            # use generator with tqdm for progress visualization
+            for line in tqdm(inp_file, total=num_lines):
+                HID.type_string(line)
+    except:
+        # encoding error
+        print('Skipping', filepath)
+
+    time.sleep(0.4)
+    vim_wq()
