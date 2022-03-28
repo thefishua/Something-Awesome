c817844c710f0d46b90b7c33f43273a468b8e6be
Joshua Fish <josh.fishua@gmail.com>
2022-03-28 23:07:11 +1100

    Implemented changing of wallpapers

--- change_wallpaper.py
+++ change_wallpaper.py
@@ -8,62 +8,40 @@ if __name__ == '__main__':
     HID.press(bytes([CODE.LEFT_CTRL, 0, CODE.ESC, *[0] * 5]))
     time.sleep(0.1)
     # open web browser with this URL
-    cmd = 'chrome http://www.thecuriosityworkshop.com/wp-content/uploads/2015/03/01-rubberduck-hongkong.jpg'
+    cmd = 'chrome http://images.hdqwalls.com/download/hacker-8k-8p-2560x1440.jpg'
     HID.type_string(cmd)
-    time.sleep(1.0)
-
+    time.sleep(0.5)
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
-    time.sleep(5.0)
-
+    time.sleep(2)
     HID.press(bytes([CODE.LEFT_CTRL, 0, CODE.KEY_S, *[0] * 5]))
-    time.sleep(1.0)
-    # save location
-    # for Chrome: %USERPROFILE%\Downloads\01-rubberduck-hongkong.jpg
-    cmd = '%USERPROFILE%\\Downloads\\01-rubberduck-hongkong.jpg'
-    HID.type_string(cmd)
-    time.sleep(0.2)
+    time.sleep(1)
+    ## save location
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
     time.sleep(0.5)
 
     # TAB to move focus to Show all Downloads (Chrome)
-    # HID.press(bytes([*[0] * 2, CODE.TAB, *[0] * 5]))
-    # time.sleep(0.2)
-    # HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
-
-    # or Ctrl + J
-    HID.press(bytes([CODE.LEFT_CTRL, 0, CODE.KEY_J, *[0] * 5]))
-    time.sleep(1.0)
-    # press tab 2 times to move focus to the latest download
-    for _ in range(2):
-        HID.press(bytes([*[0] * 2, CODE.TAB, *[0] * 5]))
-        time.sleep(0.5)
-
-    # open the image using default Windows image viewer (Photo)
+    HID.press(bytes([*[0] * 2, CODE.TAB, *[0] * 5]))
+    time.sleep(0.2)
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
-    time.sleep(3.0)
-
-    # press tab 11 times
+    time.sleep(1.0)
+    # press tab 11 times to move 
     for _ in range(11):
         HID.press(bytes([*[0] * 2, CODE.TAB, *[0] * 5]))
-        time.sleep(0.5)
-
+    time.sleep(0.2)
+    # open the settings to set to wallpaper
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
-    time.sleep(0.5)
 
-    # press down 6 times
+    ## press down 6 times
     for _ in range(6):
         HID.press(bytes([*[0] * 2, CODE.KEY_DOWN, *[0] * 5]))
-        time.sleep(0.5)
-
+        time.sleep(0.1)
+    time.sleep(0.5)
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
     time.sleep(0.5)
-
     HID.press(bytes([*[0] * 2, CODE.KEY_DOWN, *[0] * 5]))
     time.sleep(0.5)
-
     HID.press(bytes([*[0] * 2, CODE.ENTER, *[0] * 5]))
     time.sleep(0.5)
-
     # close 2 windows
     for _ in range(2):
         HID.press(bytes([CODE.LEFT_ALT, 0, CODE.F4, *[0] * 5]))
--- example.py
+++ example.py
@@ -5,14 +5,15 @@ NULL_CHAR = chr(0)
 def write_report(report):
     with open('/dev/hidg0', 'rb+') as fd:
         fd.write(report.encode())
+        print(report.encode())
 write_report(chr(0xE0)+NULL_CHAR+chr(0x29)+NULL_CHAR*5)
 
 
 write_report(NULL_CHAR*8)
-#write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
-#write_report(NULL_CHAR*2+chr(5)+NULL_CHAR*5)
-#write_report(NULL_CHAR*8)
-#write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
-#write_report(NULL_CHAR*2+chr(44)+NULL_CHAR*5)
-#write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
-#write_report(NULL_CHAR*8)
+write_report(chr(32)+NULL_CHAR+chr(4)+NULL_CHAR*5)
+write_report(NULL_CHAR*2+chr(5)+NULL_CHAR*5)
+write_report(NULL_CHAR*8)
+write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
+write_report(NULL_CHAR*2+chr(44)+NULL_CHAR*5)
+write_report(chr(32)+NULL_CHAR+chr(5)+NULL_CHAR*5)
+write_report(NULL_CHAR*8)
